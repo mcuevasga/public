f69ff8ddfb696219d9541a8496b1e822a3fbd40f
mcuevasga <marc.cuga@gmail.com>
2024-10-13 20:42:23 +0200

    llama 3 gguf

--- llm-test.py
+++ llm-test.py
@@ -69,7 +69,7 @@ class VLLMDeployment:
             #     served_model_names = self.engine_args.served_model_name
             # else:
             #     served_model_names = [self.engine_args.model]
-            base_model_paths = [BaseModelPath(name="TinyLlama-1.1B-Chat-v1.0", model_path=self.engine_args.model)]
+            base_model_paths = [BaseModelPath(name="Llama-3.2-3B-Instruct-Q8_0.gguf", model_path=self.engine_args.model)]
             self.openai_serving_chat = OpenAIServingChat(
                 self.engine,
                 model_config,
@@ -113,13 +113,13 @@ def parse_vllm_args(cli_args: Dict[str, str]):
     logger.info(arg_strings)
     parsed_args = parser.parse_args(args=arg_strings)
 
-    # model_file = "llama2_7b_chat_uncensored.Q8_0.gguf"
-    model_folder = "TinyLlama-1.1B-Chat-v1.0"
+    model_file = "Llama-3.2-3B-Instruct-Q8_0.gguf"
+    # model_folder = "TinyLlama-1.1B-Chat-v1.0"
     local_dir = "/data/models/cache/"
-    # model_model_file = f'{local_dir}{model_file}'
-    model_model_folder = f'{local_dir}{model_folder}'
+    model_model_file = f'{local_dir}{model_file}'
+    # model_model_folder = f'{local_dir}{model_folder}'
 
-    parsed_args.model = model_model_folder
+    parsed_args.model = model_model_file
     parsed_args.tensor_parallel_size = 1
     parsed_args.pipeline_parallel_size = 1
     parsed_args.gpu_memory_utilization = 0.9
