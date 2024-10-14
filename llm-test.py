79db5fa71537af1aedd036468b353c91232868d0
mcuevasga <marc.cuga@gmail.com>
2024-10-14 16:00:49 +0200

    metallama-31-8b-inst

--- llm-test.py
+++ llm-test.py
@@ -69,7 +69,7 @@ class VLLMDeployment:
             #     served_model_names = self.engine_args.served_model_name
             # else:
             #     served_model_names = [self.engine_args.model]
-            base_model_paths = [BaseModelPath(name="Meta-Llama-3.1-8B-Instruct", model_path=self.engine_args.model)]
+            base_model_paths = [BaseModelPath(name="metallama-31-8b-inst", model_path=self.engine_args.model)]
             self.openai_serving_chat = OpenAIServingChat(
                 self.engine,
                 model_config,
@@ -114,7 +114,7 @@ def parse_vllm_args(cli_args: Dict[str, str]):
     parsed_args = parser.parse_args(args=arg_strings)
 
     # model_file = "Llama-3.2-3B-Instruct-Q8_0.gguf"
-    model_folder = "Meta-Llama-3.1-8B-Instruct"
+    model_folder = "metallama-31-8b-inst"
     local_dir = "/data/models/cache/"
     # model_model_file = f'{local_dir}{model_file}'
     model_model_folder = f'{local_dir}{model_folder}'
